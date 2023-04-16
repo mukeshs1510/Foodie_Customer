261969d9f3613575bef2d100373b9f10537db90c
mukeshs1510 <msuthar4y@gmail.com>
2023-04-16 16:24:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/soniyo/android/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.soniyo.android
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity()
